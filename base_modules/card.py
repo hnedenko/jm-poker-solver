9f586b61cd4caa47e6f6e70711639bb0f790eda4
hnedenko <73238004+hnedenko@users.noreply.github.com>
2025-06-13 10:10:11 +0300

    add Card class

---
+++ base_modules/card.py
@@ -0,0 +1,42 @@
+import enum
+import random
+
+
+class Suit(enum.Enum):
+    spades = 7 # пики
+    hearts = 5 # черви
+    diamonds = 3 # бубны
+    clubs = 2 # трефы
+
+    def __str__(self):
+        return self.name
+
+
+class Value(enum.Enum):
+    two = 11
+    three = 13
+    four = 17
+    five = 19
+    six = 23
+    seven = 29
+    eight = 31
+    nine = 37
+    ten = 41
+    jack = 43
+    queen = 47
+    king = 53
+    ace = 59
+
+    def __str__(self):
+        return self.name
+
+
+class Card:
+    def __init__(self, suit=random.choice(list(Suit)), value=random.choice(list(Value))):
+        self.suit = suit
+        self.value = value
+
+    def __str__(self):
+        str_state = '(' + str(self.suit) + ' ' + str(self.value) + ')'
+
+        return str_state

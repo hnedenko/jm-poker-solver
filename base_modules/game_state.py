914e2df5d6dceebcbdabe557749c3da2f3dce0e6
hnedenko <73238004+hnedenko@users.noreply.github.com>
2025-06-13 10:11:33 +0300

    add GameState class

---
+++ base_modules/game_state.py
@@ -0,0 +1,17 @@
+class GameState:
+    def __init__(self):
+        self.player_cards = [None, None]
+        self.board_cards = [None, None, None, None, None]
+        self.n_players = None
+        self.pot = None
+        self.bet = None
+
+    def __str__(self):
+        str_state = "Player cards:" + str(self.player_cards) + '\n'
+        str_state = str_state + "Board cards:" + str(self.board_cards) + '\n'
+        str_state = str_state + "N players:" + str(self.n_players) + '\n'
+        str_state = str_state + "Pot:" + str(self.pot) + '\n'
+        str_state = str_state + "Bet:" + str(self.bet) + '\n'
+        str_state = str_state + "Solve available:" + str(self.is_solve_available())
+
+        return str_state
